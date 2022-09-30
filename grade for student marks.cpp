c3fcb1d517a5fc5dd3b9223840e6b8b1b8ce13fe
Nara akshaya choudary <112068968+Akshayanara12@users.noreply.github.com>
2022-09-30 08:43:18 +0530

    Add files via upload

--- grade for student marks.cpp
+++ grade for student marks.cpp
@@ -1,35 +1,38 @@
 #include<stdio.h>
 int main()
 {
-    int marks;
-    printf("Enter your marks ");
-    scanf("%d",&marks);
-    if(marks<0 || marks>100)
-    {
-        printf("Wrong Entry");
-    }
-    else if(marks<50)
-    {
-        printf("Grade F");
-    }
-    else if(marks>=50 && marks<60)
-    {
-        printf("Grade D");
-    }
-    else if(marks>=60 && marks<70)
-    {
-        printf("Grade C");
-    }
-    else if(marks>=70 && marks<80)
-    {
-        printf("Grade B");
-    }
-    else if(marks>=80 && marks<90)
-    {
-        printf("Grade A");
-    }
-    else
-    {
-        printf("Grade A+");
-    }
+	int m1,m2,m3,m4,total,avg;
+	printf("Enter the marks in Python: ");
+	scanf("%d", &m1);
+	printf("\nEnter the marks in C Programming: ");
+	scanf("%d", &m2);
+	printf("\nEnter the marks in Mathematics: ");
+	scanf("%d", &m3);
+	printf("\nEnter the marks in Physics: ");
+	scanf("%d", &m4);
+	total=m1+m2+m3+m4;
+	printf("\nThe Total is: %d", total);
+	avg=total/4;
+	printf("\nThe aggregate is: %d", avg);
+	if(avg>=75)
+	{
+		printf("\nDistinction");
+	}
+	else if(avg>=60&&avg<75)
+	{
+		printf("\nFirst Class");
+	}
+	else if(avg>=50&&avg<60)
+	{
+		printf("\nSecond Class");
+	}
+	else if(avg>=40&&avg<50)
+	{
+		printf("\nThirsd Class");
+	}
+	else
+	{
+		printf("Fail");
+	}
+	return 0;
 }
